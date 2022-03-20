d01883d5da1628a40f3edc9289db5ff4a7e31935
Qais Khan <q5khan@uwaterloo.ca>
2022-03-19 21:53:38 -0400

    Mongo integrated

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 import time
 import discord
 import calendar
+import pymongo
 
 from datetime import date
 from discord.ext import commands
@@ -17,22 +18,33 @@ from pandas import describe_option
 
 
 
+
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-ToDoList = {
-  "Monday" : [],
-  "Tuesday" : [],
-  "Wednesday" : [],
-  "Thursday" : [],
-  "Friday" : [],
-  "Saturday" : [],
-  "Sunday" : []
-}
+ToDoList = [
+  {"Monday" : ['pain'],'index':0},
+  {"Tuesday" : ['pain'],'index':1},
+  {"Wednesday" : [],'index':2},
+  {"Thursday" : [], 'index':3},
+  {"Friday" : [],'index':4},
+  {"Saturday" : [],'index':5},
+  {"Sunday" : [],'index':6}
+]
 
 load_dotenv()
+MONGO_KEY = os.getenv('MONGO_KEY')
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='~')
+bot = commands.Bot(command_prefix='-')
+client = pymongo.MongoClient(MONGO_KEY)
+db = client.test
 
+if "To_Do" not in db.list_collection_names():
+    db.create_collection("To_Do")
+    toDo = db.To_Do
+    toDo.insert_many(ToDoList)
+
+
+toDo = db.To_Do
 
 
 def is_user(ctx):
@@ -40,9 +52,9 @@ def is_user(ctx):
 
 @bot.event
 async def on_ready():
-    channel = bot.get_channel(944347370739605555)
+    # channel = bot.get_channel(944347370739605555)
     print(f'{bot.user.name} has connected to Discord!')
-    await channel.send("WTF Do You Want From Me")
+    # await channel.send("WTF Do You Want From Me")
 
 @bot.command(name='Suby', help='Tells the Truth')
 async def suby(ctx):
@@ -55,7 +67,9 @@ async def suby(ctx):
         'Who?',
         'Imagine Being a BME defect lol',
         'Such a simp (Courtesy of Lucas)',
-        'Silly Suby'
+        'Silly Suby',
+        'Pretty Pog <:poggy:934688467538030622>',
+        '<:lmaocry:933424582805827634>'
     ]
     response = random.choice(Wisdom)
     await ctx.send(response)
@@ -77,7 +91,7 @@ async def jokes(ctx):
     embed=discord.Embed(title="Commands", color=discord.Color.purple())
     embed.add_field(name="Joke", value="Tells A Great Dad Joke", inline=False)
     embed.add_field(name="Suby", value="Tells The Cold Hard Truth",inline=False)
-    embed.add_field(name="Today", value="Lets You Know What's Due Today!", inline=False) 
+    embed.add_field(name="DueToday", value="Lets You Know What's Due Today!", inline=False) 
     embed.add_field(name="Due", value="Lets You Know What's Due On A Given Day!",inline=False) 
     embed.set_footer(text="Help requested by: {}".format(ctx.author.display_name))
     await ctx.send(embed=embed)
@@ -108,7 +122,7 @@ async def error(ctx, error):
 
 
 
-@bot.command(name="DescSet", help="Shows What Is Due Today!")
+@bot.command(name="DescSet")
 @commands.check(is_user)
 async def desc(ctx, *args):
     global description
@@ -119,9 +133,15 @@ async def desc(ctx, *args):
 @commands.check(is_user)
 async def ToDoSet(ctx, *args):
     day = args[0].title()
+    counter = 0
+    for x in days:
+        if day == x:
+            break
+        counter +=1
     task = ' '.join(args[1:])
-    ToDoList[day].append(task)
-    print(ToDoList[day])
+    toDo.update_one({'index': counter}, {'$push': {day: task}})
+    cursor = toDo.find()[counter]
+    print(cursor[day])
     await ctx.send("Done")
 
 @bot.command(name="ToDoClear", help="Set The Deliverables Due Each Day")
@@ -147,11 +167,17 @@ async def Today(ctx):
 @bot.command(name="Due", help="Shows What Is Due On a Given Day!")
 async def Today(ctx, day):
     day = day.title()
-    embed=discord.Embed(title="What's Due " + day + " ?", color=discord.Color.purple())
-    counter = 1
-    for x in range(len(ToDoList[day])):
-        embed.add_field(name= str(counter) + ":", value=ToDoList[day][x], inline=False)
-        counter+=1
+    counter = 0
+    for x in days:
+        if day == x:
+            break
+        counter +=1
+    cursor = toDo.find()[counter]
+    desc = []
+    for x in cursor[day]:
+        desc.append(x + '\n')
+    description = ' '.join(desc)
+    embed=discord.Embed(title="What's Due " + day + " ?", description = description, color=discord.Color.purple())
     embed.set_footer(text="Requested by: {}".format(ctx.author.display_name))
     await ctx.send(embed=embed)
 
@@ -172,16 +198,24 @@ async def ToDoThisWeek(ctx):
     await channel.send(embed=embed)
 
 @bot.command(name='P', help='Hide your plays')
-async def clear(ctx, amount=4):
+async def clear(ctx, amount=3):
     await ctx.channel.purge(limit=amount)
 
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
-    if message.content.capitalize() in ['Gn zephy <:lil_poggy:934689146344190033>', 'Gn zephy <:poggy:934688467538030622>', 'Gn zephy',]:
+    if 'Gn zephy' in message.content.capitalize() :
         await message.reply('Gn ' + str(message.author.display_name) + "!")
     if '.move' in message.content:
-            await message.channel.purge(limit=4)
-        
+        await message.channel.purge(limit=3)
+    if '!move' in message.content:
+        time.sleep(0.25)
+        await message.channel.purge(limit=4)
+    if 'Hi zephy' in message.content.capitalize() :
+        await message.reply('Hi ' + str(message.author.display_name) + "! <:poggy:934688467538030622>")
+    if 'Hey zephy' in message.content.capitalize() :
+        await message.reply('Hey ' + str(message.author.display_name) + "! <:poggy:934688467538030622>")
+
+
 
 bot.run(TOKEN)
